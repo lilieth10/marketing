794eac714ed819e9df867fd5573cdc759d1e9b61
unknown <Jorgemathez14@gmail.com>
2025-07-06 16:01:42 -0300

    responsive navbar and new component comunity Fedd

---
+++ Ganway/app/dashboard/client/community/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import CommunityFeed from "../../../../components/dashboard/CommunityFeed"
+
+function CommunityPage() {
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <CommunityFeed/>
+    </div>
+  )
+}
+
+export default CommunityPage;
